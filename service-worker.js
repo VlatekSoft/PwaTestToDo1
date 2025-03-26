76413972f80ef9c17a92a90a1f952966d9ee67df
VlatekSoft <34581167+VlatekSoft@users.noreply.github.com>
2025-03-27 00:51:47 +0700

    фикс адресной строки

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ const urlsToCache = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+  './.well-known/assetlinks.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
